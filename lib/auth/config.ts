4931694a66f7bd9c02e933247ec5af2d2a9ae7f9
new-business-ingresse <newbusiness@ingresse.com>
2026-02-05 15:29:28 -0300

    Add Next-Auth config

---
+++ lib/auth/config.ts
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      authorize: async (credentials) => {
+        if (
+          credentials.username === process.env.ADMIN_USERNAME &&
+          credentials.password === process.env.ADMIN_PASSWORD
+        ) {
+          return {
+            id: "1",
+            name: "Admin",
+            email: "admin@ingresse.com"
+          };
+        }
+        return null;
+      }
+    })
+  ],
+  pages: {
+    signIn: "/login"
+  },
+  session: {
+    strategy: "jwt"
+  },
+  callbacks: {
+    authorized: async ({ auth }) => {
+      return !!auth;
+    }
+  }
+});
